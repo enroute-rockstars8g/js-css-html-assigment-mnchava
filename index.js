1ccee6e77d4701de730de7aad6d61df7c10473e4
mnchava <mnchava@outlook.com>
2022-04-05 15:55:05 -0500

    .

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 var img = new Image();
-img.src = "/assets/profile.jpg";
+img.src = "../assets/profile.jpg";
 img.setAttribute("class", "profile");
-document.getElementById("header").insertBefore(img, document.getElementById("header").firstChild);
+document
+  .getElementById("header")
+  .insertBefore(img, document.getElementById("header").firstChild);
 
 const center_x = img.offsetLeft + img.width / 2;
 const center_y = img.offsetTop + img.height / 2;
 
-function mouse(evt) {
-  var mouse_x = evt.pageX;
-  var mouse_y = evt.pageY;
-
-  var radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
+document.onmousemove = function (event) {
+  var radians = Math.atan2(event.pageX - center_x, event.pageY - center_y);
   var degree = radians * (180 / Math.PI) * -1 + 90;
 
   img.style.transform = "rotate(" + degree + "deg)";
-}
-
-img.onmousemove = mouse;
+};
