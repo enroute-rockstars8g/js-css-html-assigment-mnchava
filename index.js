445739a75f1bc7f8d95ed8248c55a1a86e487e51
mnchava <mnchava@outlook.com>
2022-04-05 19:45:22 -0500

    Added bio, styles, and funcionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var img = new Image();
 img.src = "../assets/profile.jpg";
-img.setAttribute("class", "profile");
+img.className = "profile";
+
 document
   .getElementById("header")
   .insertBefore(img, document.getElementById("header").firstChild);
@@ -14,3 +15,41 @@ document.onmousemove = function (event) {
 
   img.style.transform = "rotate(" + degree + "deg)";
 };
+
+var fiboInput = document.querySelector("#inputFibo");
+var factInput = document.querySelector("#inputFact");
+
+var fiboButton = document.querySelector("#calculateFibo");
+fiboButton.onclick = function () {
+  fiboInput.value = findIndex(parseInt(fiboInput.value)).toString();
+};
+
+var factButton = document.querySelector("#calculateFact");
+factButton.onclick = function () {
+	factInput.value = getFactorial(parseInt(factInput.value));
+};
+
+function findIndex(givenNumber) {
+  if (givenNumber <= 1) return givenNumber;
+
+  let [a, b, currentNumber, res] = [0, 1, 1, 1];
+
+  while (currentNumber < givenNumber) {
+    currentNumber = a + b;
+
+    res++;
+    a = b;
+    b = currentNumber;
+  }
+  return res;
+}
+
+function getFactorial(n) {
+  var memo = {};
+
+  function factorial(n) {
+    return n < 2 ? 1 : memo[n] || (memo[n] = n * factorial(n - 1));
+  }
+
+  return factorial(n);
+}
